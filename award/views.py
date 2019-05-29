3b63bcbf5eeba1e87d1a2dcf86a1d746c3fa1027
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-29 12:54:45 +0300

    add a view function for search form

--- award/views.py
+++ award/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views import generic
 from django.db.models import F
 from users.models import Profile
 from . models import Project
+from django.contrib.auth.models import User
+
 
 def welcome(request):
     return render(request, 'award/welcome.html')
 
-class ProjectCreateView(LoginRequiredMixin, CreateView):
+class ProjectListView(ListView):
+    model = Project
+    # template_name = 'award/home.html' 
+    # context_object_name = 'images'
+    ordering = ['-timestamp']
+
+class UserProjectListView(ListView):
+    model = Project
+    context_object_name = 'object'
+    template_name = 'award/user_projects.html'
+    ordering = ['-timestamp']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Project.objects.filter(poster=user).order_by('-timestamp')
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+class ProjectCreateView(CreateView):
     model = Project
     fields = ['img','title','description','link']
 
@@ -18,6 +40,21 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
         form.instance.poster = self.request.user
         return super().form_valid(form)
 
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'award/search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'award/search.html',{"message":message})
+
+def about(request):
+    return render(request, 'award/about.html')
 
 # @login_required
 # def profile(request):
