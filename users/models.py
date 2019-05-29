863a8e93a22891290e73018c5ef416c1331961ff
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-29 09:13:01 +0300

    add users app

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(default='Available', max_length = 30)
+    image = models.ImageField(default='kitchen.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+    # overriding save and resizing then saving
+    # def save(self,*args,**kwargs):
+    #     super().save(*args,**kwargs)
+
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
