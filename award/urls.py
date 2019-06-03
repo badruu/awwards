919a983f06e441ecc260bbdfd2515a991be81a49
projects <projects@Salaaxs-MacBook-Air.local>
2019-06-03 13:29:00 +0300

    add urls for api views, rating and review

--- award/urls.py
+++ award/urls.py
@@ -7,12 +7,22 @@ from .views import ProjectCreateView,ProjectListView,ProjectDetailView,UserProje
 
 
 urlpatterns=[
-    # url(r'^$', views.welcome, name = 'award-home'),
+    url(r'^welcome$', views.welcome, name = 'award-welcome'),
+    url(r'^home$', views.home, name = 'home'),
+    url(r'^review/(\d+)/$',views.review,name='review'),
+    url(r'^rate/(\d+)/$',views.rate,name='rate'),
+    url(r'^accounts/profile/$', views.profile, name = 'profile'),
+    url(r'^api/profiles/$', views.ProfilesList.as_view()),
+    url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileDescription.as_view()),
+    url(r'^api/projects/$', views.ProjectsList.as_view()),
+    url(r'api/project/project-id/(?P<pk>[0-9]+)/$',views.ProjectDescription.as_view()),
     url(r'^$', ProjectListView.as_view(), name = 'award-home'),
     url(r'^project/new/$',ProjectCreateView.as_view(), name='project-create'),
     url(r'^user/(?P<username>\w+)/$', UserProjectListView.as_view(), name='user-projects'),
-    url(r'^project/(?P<pk>\d+)/',ProjectDetailView.as_view(), name='project-detail'),
+    url(r'^project/(?P<pk>\d+)/$',ProjectDetailView.as_view(), name='project-detail'),
+    # url(r'^project/(?P<pk>\d+)/update/',ProjectUpdateView.as_view(), name='project-update'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^project/(?P<pk>\d+)/rating', views.rating, name='award-rating'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
